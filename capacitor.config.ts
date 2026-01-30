842e86011af69df94993ef3ea3ef67652b09c168
tazpaul6631 <58732828+tazpaul6631@users.noreply.github.com>
2026-01-30 10:17:59 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vuetify.project',
+  appName: 'vuetify-project',
+  webDir: 'dist'
+};
+
+export default config;
